cb838684f6229dc644ff1c64e883fdc351ef2199
ERKELSOT <1256029807@qq.com>
2022-07-26 20:13:59 +0800

    ✨ feat(packages): add some packages

--- packages/vite-cli/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.a = void 0;
-var a = 123;
-exports.a = a;
-console.log(a);
-console.log(a + 1);
--- packages/vite-cli/index.ts
+++
@@ -1,4 +0,0 @@
-const a = 123
-console.log(a)
-console.log(a + 1)
-export { a }
